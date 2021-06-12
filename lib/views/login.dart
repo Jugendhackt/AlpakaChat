399ce09472ce2c6a9559760f66eae7ee9a6d0ebd
BuildTools <unconfigured@null.spigotmc.org>
2021-06-12 13:35:47 +0200

    Login Window

--- lib/views/login.dart
+++ lib/views/login.dart
@@ -10,12 +10,47 @@ class Loginwidget extends StatefulWidget{
 class _loginwidget extends State<Loginwidget>{
   @override
   Widget build(BuildContext context) {
-    return ListView(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      mainAxisSize: MainAxisSize.max,
       children: [
-        Text("Login", style: TextStyle(fontSize: 50),),
-        Text("Email:", style: TextStyle(fontSize: 20),),
-        TextField(
-
+        Text("Login", style: TextStyle(fontSize: 50), textAlign: TextAlign.center),
+        SizedBox(
+          width: 300,
+          child: TextField(
+            decoration: InputDecoration(
+                hintText: "Email",
+                contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                border: OutlineInputBorder()
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 300,
+          child: TextField(
+            decoration: InputDecoration(
+                hintText: "Password",
+                contentPadding: EdgeInsets.symmetric(vertical: 20, horizontal: 10),
+                border: OutlineInputBorder()
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 200,
+            height: 50,
+            child :OutlinedButton(
+              child: Text("Submit", style: TextStyle(fontSize: 20),),
+              style: ButtonStyle(
+                  foregroundColor: MaterialStateProperty.all(Colors.white),
+                  shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                      RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(50),
+                          side: BorderSide(color: Colors.white)
+                      )
+                  )
+              ),
+              onPressed: (){},
+            )
         )
       ],
     );
