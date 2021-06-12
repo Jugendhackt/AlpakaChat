69ece05ad01177deaf5d07c4db6ddd1db96894f2
Felix <info@strifel.de>
2021-06-12 10:44:29 +0200

    :tada: Add project structure

---
+++ android/app/src/main/kotlin/de/jugendhackt/alpakachat/alpaka_chat/MainActivity.kt
@@ -0,0 +1,6 @@
+package de.jugendhackt.alpakachat.alpaka_chat
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Alpaka Chat',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Hello World"),
+    );
+  }
+
+}
