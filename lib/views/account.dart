dfc772f0c837aa3b9988d2442527ae6eeb5871b0
Konradrundfunk <33392939+Konradrundfunk@users.noreply.github.com>
2021-06-13 12:33:58 +0200

    account dummy page

--- lib/views/account.dart
+++ lib/views/account.dart
@@ -10,8 +10,37 @@ class _settingsviewstate extends State<Settingsview>{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Accounts"),
+      ),
+      body: ListView(
+        children: [
+          Card(
+            child: Padding(padding: EdgeInsets.all(20),
+            child: Center(
+                child: Column(
+                  children: [
+                    Text(
+                      "Accountname",
+                      style: TextStyle(
+                        fontWeight: FontWeight.w600,
+                        fontSize: 30,
+                      ),
+                    ),
+                    SizedBox(height: 30,),
+                    Image.network('https://picsum.photos/250?image=9')
 
+                  ],
+                ),
+              )
+            ),
+          )
+        ],
+      ),
     );
   }
-
 }
+
+/*_getaccountname(){
+  return FutureBuilder(builder: builder)
+}*/
