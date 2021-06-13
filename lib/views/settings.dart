7536f803ce0cd44151ab0d89892fde1194e9d48b
Konradrundfunk <33392939+Konradrundfunk@users.noreply.github.com>
2021-06-13 10:03:10 +0200

    changed server adress

---
+++ lib/views/settings.dart
@@ -0,0 +1,17 @@
+
+
+import 'package:flutter/cupertino.dart';
+
+class Settingsview extends StatefulWidget{
+  @override
+  _settingsviewstate createState() => _settingsviewstate();
+  
+}
+
+class _settingsviewstate extends State<Settingsview>{
+  @override
+  Widget build(BuildContext context) {
+    return Text("hi");
+  }
+  
+}
