47325e2dff798c0545fdf00adddab48af355edc4
Felix <info@strifel.de>
2021-06-13 10:22:01 +0200

    :sparkles: Add basic reloading functionality

--- lib/views/chatpage.dart
+++ lib/views/chatpage.dart
@@ -18,35 +18,51 @@ class ChatPage extends StatelessWidget {
         title: Text("Chat"),
         centerTitle: true,
       ),
-      body: FutureBuilder(
-        future: Matrix.of(context).client.getRoomEvents(room.id, room.prev_batch, Direction.b, limit: 200),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) return Text(snapshot.error.toString());
-          if (!snapshot.hasData) return CircularProgressIndicator();
-          TimelineHistoryResponse historyResponse = snapshot.data as TimelineHistoryResponse;
-          Iterable<MatrixEvent> events = historyResponse.chunk.reversed;
-          List<Widget> messageView = events.map((event) {
-            if (event.type == "m.room.message") {
-              return ListTile(
-                title: Text("${event.senderId}"),
-                subtitle: Text("${event.content['body']}"),
-              );
-            } else if (event.type == "m.room.encrypted") {
-              return ListTile(
-                leading: Icon(Icons.warning_amber_outlined),
-                title: Text("Die Nachricht ist verschlüsselt"),
-                subtitle: Text("Leider kann AlpakaChat noch keine Nachrichten entschlüsseln"),
-              );
-            }
-            return Container();
-          }).toList();
-          messageView.add(Divider());
-          messageView.add(SendMessageWidget(room));
-          return ListView(
-            children: messageView,
-          );
-        },
+      body: StreamBuilder(
+        stream: Matrix.of(context).client.onEvent.stream,
+        builder: (context, snapshot) => _MessageView(room),
       ),
+    );
+  }
+
+}
+
+class _MessageView extends StatelessWidget {
+
+  Room _room;
+
+  _MessageView(this._room);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Matrix.of(context).client.getRoomEvents(_room.id, _room.prev_batch, Direction.b, limit: 200),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) return Text(snapshot.error.toString());
+        if (!snapshot.hasData) return CircularProgressIndicator();
+        TimelineHistoryResponse historyResponse = snapshot.data as TimelineHistoryResponse;
+        Iterable<MatrixEvent> events = historyResponse.chunk.reversed;
+        List<Widget> messageView = events.map((event) {
+          if (event.type == "m.room.message") {
+            return ListTile(
+              title: Text("${event.senderId}"),
+              subtitle: Text("${event.content['body']}"),
+            );
+          } else if (event.type == "m.room.encrypted") {
+            return ListTile(
+              leading: Icon(Icons.warning_amber_outlined),
+              title: Text("Die Nachricht ist verschlüsselt"),
+              subtitle: Text("Leider kann AlpakaChat noch keine Nachrichten entschlüsseln"),
+            );
+          }
+          return Container();
+        }).toList();
+        messageView.add(Divider());
+        messageView.add(SendMessageWidget(_room));
+        return ListView(
+          children: messageView,
+        );
+      },
     );
   }
 
